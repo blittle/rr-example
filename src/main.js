6409b4ef68e5aa19eda6a5d5844d225488181dea
Bret Little <bret.little@gmail.com>
2016-04-16 06:49:57 -0600

    2

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import HelloWorld from './HelloWorld.js';
 
-render(<HelloWorld/>, document.querySelector('#app'));
+import { Router, Route, hashHistory } from 'react-router'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={HelloWorld}/>
+    <Route path="/two" component={NewRoute}/>
+  </Router>
+), document.getElementById('app'))
